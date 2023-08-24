2543d6cd7adf402afef76fd4976db02d8fafb2eb
czhu-24 <czhu@LABA.PSWAS.LOCAL>
2023-08-24 08:49:45 -0700

    no idea what changes i made to index.js should probably work on that lol

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const addBtnEl = document.getElementById("add-task");
 const resetBtnEl = document.getElementById("reset-tasks");
 const listEl = document.getElementsByClassName("list")[0];
 const inputFieldEl = document.querySelector("input");
+const completedListEl = document.getElementsByClassName("completed-list")[0];
 
 const tasksArray = [];
 
@@ -44,9 +45,33 @@ const resetTasks = () => {
 
 const completeTask = (e) => {
     // get the checkbox's parent node to remove itself from DOM (along w/ its children)
+
+    let id = e.target.getAttribute("id");
+    let idNumber = id.substring(3, id.length);
+
+    // set the corresponding task in tasksArray so that its isCompleted property is true
+    tasksArray[idNumber].completed = true;
+    console.log(idNumber);
+
+    // take the 
+    e.target.parentNode.style.display = "none";  
+
+    // toggle completed list to display if it's not already
+    //toggleCompletedList();
+}
+
+const toggleCompletedList = () => {
+    // append completed task to the completed list
+    // HM... the HTML tag still exists
+    const completedTask = document.getElementById("id-0");
     
-    e.target.parentNode.style.display = "none";
-    console.log(e.target.parentNode.display);
+
+    // toggle
+    if(completedListEl.style.display === "none") {
+        completedListEl.style.display = "grid";
+    }else{
+        completedListEl.style.display = "none";
+    }
 }
 
 addBtnEl.addEventListener('click', addTask);
